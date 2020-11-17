5a924461bc8d509878b38c8828d9daae6bab4aea
ChrisLee <christopher.dan.lee96@gmail.com>
2020-11-17 00:32:35 -0500

    user_auth_half

---
+++ frontend/components/greeting/greeting.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Greeting = ({currentUser, logout}) => {
+    const sessionLinks = () => (
+        <nav className= "login-signup">
+            <Link to = "/login">Login</Link>
+            &nbsp;&nbsp;
+            <Link to ="/signup">Sign up!</Link>
+        </nav>
+    )
+
+    const signedIn = () => (
+    <div>
+        <h1>Hi {currentUser.username}</h1>
+            <button onClick = {logout}>Log Out Here!</button>
+    </div>
+    )
+
+    return currentUser ? signedIn() : sessionLinks();
+}
+
+export default Greeting;
+
--- frontend/grabbin_good.jsx
+++ frontend/grabbin_good.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from './store/store';
+import Root from './components/root';
+
+//testing
+import { login } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>This is Grabbin Good!!!!</h1>, root);
+    const store = configureStore();
+
+    //testing only
+    window.dispatch = store.dispatch;
+    window.getState = store.getState;
+    window.login = login;
+
+    
+    ReactDOM.render(<Root store = {store}/>, root);
+
+   
+    
 });
